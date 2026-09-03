15a8cf5e0a7708b654a48990c455817fb5b8cd94
TSarca30 <marco.arcangeletti13@gmail.com>
2026-09-03 18:35:12 +0200

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,1 @@
-import './globals.css'
 
-export const metadata = {
-  title: 'Asta Live Fantacalcio Mantra',
-  description: 'Gestione Asta Live Mantra con Google Sheets',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="it">
-      <body>{children}</body>
-    </html>
-  )
-}
